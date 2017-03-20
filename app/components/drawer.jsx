9cb326fbbea69b9dd1fe1a4be013fa089dc00f79
Alamgir Qazi <alamgirmunirqazi@gmail.com>
2017-03-20 12:59:17 +0500

    added close button to drawer

--- app/components/drawer.jsx
+++ app/components/drawer.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import Drawer from 'material-ui/Drawer';
 import AppBar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
+import IconButton from 'material-ui/IconButton';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import FlatButton from 'material-ui/FlatButton';
 
 export default class DrawerOpenRight extends React.Component {
+
 
   constructor(props) {
     super(props);
     this.state = {open: false};
   }
+
+
+
 
   handleToggle = () => this.setState({open: !this.state.open});
 
@@ -20,9 +27,20 @@ export default class DrawerOpenRight extends React.Component {
           onTouchTap={this.handleToggle}
         />
         <Drawer width={570} openSecondary={true} open={this.state.open} >
-          <AppBar title="This is a drawer" />
+          <AppBar title="This is a drawer"
+
+  iconElementLeft={<IconButton onClick={this.handleToggle}><NavigationClose /></IconButton>}
+     iconElementRight={<FlatButton label="Save" />}
+           />
+
+
         </Drawer>
       </div>
     );
   }
 }
+              {/*onTitleTouchTap={this.handleToggle}*/}
+
+
+//  {/*iconElementLeft={<IconButton><NavigationClose /></IconButton>}*/}
+//     {/*iconElementRight={<FlatButton label="Save" />}*/}
